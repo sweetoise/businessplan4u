00378230cc21a5d7ceb878363cf9d2ba04d8c8a5
sweetoise <sweetoise@ymail.com>
2016-03-23 05:42:21 +0100

    Fixing Titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Mec Consulting"
+    base_title = "MeConsulting"
     page_title.empty? ?  base_title : "#{base_title} :: #{page_title}"
   end
 end
